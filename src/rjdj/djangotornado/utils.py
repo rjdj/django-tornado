9585ac35692845c9ff09fdc74819f3f6a6d6d7d1
claus <claus@rjdj.me>
2011-06-06 17:50:01 +0100

    fixed a typo

--- src/rjdj/djangotornado/utils.py
+++ src/rjdj/djangotornado/utils.py
@@ -8,7 +8,7 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# django-tornado is distributed in the hope that it will be useful,
+# Django-tornado is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
