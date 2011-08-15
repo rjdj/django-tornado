95febc7385b64c027349a2c3074d1d9508a4cdfb
claus <claus@rjdj.me>
2011-08-15 19:04:44 +0100

    incremented version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name = "rjdj.djangotornado",
-      version = "0.2.5d2",
+      version = "0.2.5d3",
       author = 'Reality Jockey Limited',
       author_email = 'developer@rjdj.me',
       description = 'Use Tornado with your Django project.',
